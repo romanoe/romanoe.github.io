012c0396f7750ab0d63cbe822b42d32c38a13fa0
romanoe <noemi.romano@protonmail.com>
2022-02-04 15:39:29 +0100

    remove dist folder

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env sh
-
-
 # abort on errors
 set -e
 
-# build
+# build & deploy
 npm run build
-
-#deploy
-git add dist
-git commit -m 'adding dist subtree'
-git subtree push --prefix dist origin gh-pages
+gh-pages -d dist
